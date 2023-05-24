98998748431a7753ba25289295dd1582b9b221c5
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-24 18:51:21 +0000

    Wed, May 24, 2023, 2:51 PM -04:00

--- hello/hello.c
+++ hello/hello.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+    // Get name as a string from user
     string name = get_string("What's your name? ");
+    // Print Hello so-and-so where so-and-so is the user's name
     printf("hello, %s\n", name);
 }
