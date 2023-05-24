89cd56ba2ee221afc63004d1e29768050e6396d9
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-24 20:07:32 +0000

    Wed, May 24, 2023, 4:07 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // Get the numbers as long from the user.
+    long N;
+    do
+    {
+        N = get_long("Numbers: ");
+    }
+    while (N < 1 | n > 8);
 }
