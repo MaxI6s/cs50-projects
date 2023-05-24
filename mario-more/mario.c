658d96b712a650a14d5389a524c146d618aaab46
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-24 19:59:44 +0000

    Wed, May 24, 2023, 3:59 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -15,28 +15,20 @@ int main(void)
     int i;
     for (i = 0; i < n; ++i)
     {
-        // Initialize 2 strings of size n. One for each side of the pyramid.
-        char str1[n];
-        char str2[n];
         int k;
-
-        // Initialize each string as a string full of '#'
-        for (k = 0; k < n; ++k)
+        for (k = 0; k < n - i; ++k)
         {
-            str1[k] = '#';
-            str2[k] = '#';
+            printf(" ");
         }
-        str1[n] = '\0';
-        str2[n] = '\0';
-
-        // Rcheck50 cs50/problems/2023/x/mario/moreeplace '#' by ' ' in fonction of the line's height
-        for (k = 0; k < n - i - 1; ++k)
+        for (k = 0; k <= i; ++k)
         {
-            str1[k] = ' ';
-            str2[n - 1 - k] = ' ';
+            printf("#");
         }
-
-        // Print the 2 string separated by 2 ' '
-        printf("%s  %s\n", str1, str2);
+        printf("  ");
+        for (k = 0; k <= i; ++k)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
 }
