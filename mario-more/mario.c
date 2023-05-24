baf435eeff44badcfd29f4ab0d5a8a4e6d0a603e
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-24 20:04:12 +0000

    Wed, May 24, 2023, 4:04 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -9,22 +9,29 @@ int main(void)
     {
         n = get_int("Height: ");
     }
-    while(n < 1 | n > 8);
+    while (n < 1 | n > 8);
 
     // For each line of the pyramid.
     int i;
     for (i = 0; i < n; ++i)
     {
+        // Fill the start of the line with blank
         int k;
         for (k = 0; k < n - 1 - i; ++k)
         {
             printf(" ");
         }
+
+        // Fill the rest with '#'
         for (k = 0; k <= i; ++k)
         {
             printf("#");
         }
+
+        // Make the separation '  '
         printf("  ");
+
+        // Print the right-hand side of the pyramid
         for (k = 0; k <= i; ++k)
         {
             printf("#");
