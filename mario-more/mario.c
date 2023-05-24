251b361e4e50ba1c463f9245adaf2d477519972b
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-24 19:05:48 +0000

    Wed, May 24, 2023, 3:05 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -19,7 +19,7 @@ int main(void)
         {
             str.append("#");
         }
-        str.append(" ");
+        str.append("  ");
         str.append(str);
         printf("%s/n", str);
     }
