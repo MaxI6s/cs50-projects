0f96d7eab0d7061ee799521485f37f79493f3697
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-24 20:22:24 +0000

    Wed, May 24, 2023, 4:22 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,4 +6,31 @@ int main(void)
     // Get the numbers as long from the user.
     long N;
     N = get_long("Numbers: ");
+
+    long n = N;
+    int d = 0;
+
+    while(n != 0)
+    {
+        n = n/10;
+        d++;
+    }
+
+    if (d != 16)
+    {
+        printf("INVALID\n");
+    }
+
+    n = N;
+    while(n != 0)
+    {
+        int r;
+        r = n%10;
+        n = n/10;
+        d++;
+        printf("%i\n", r);
+    }
+
+
+
 }
