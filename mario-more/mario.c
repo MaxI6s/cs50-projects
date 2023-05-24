e3c23c12191a6dd3231263fe8b0069203b4238f4
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-24 18:58:59 +0000

    Wed, May 24, 2023, 2:58 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while(n<1 | n>8);
+
+    int i;
+    for (i=0, i<n, ++i)
+    {
+        string string
+    }
+
 }
