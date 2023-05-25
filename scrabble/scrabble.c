36ae5aeea43f489441c835888cde67f02eb5923f
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-25 13:48:15 +0000

    Thu, May 25, 2023, 9:48 AM -04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,7 +8,7 @@ int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 
 
 int compute_score(string word);
 int get_id(char c);
-int print_result(int score1, int score2);
+void print_result(int score1, int score2);
 
 int main(void)
 {
@@ -20,7 +20,8 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    print_result(int score1, int score2);
+    // Print the result
+    print_result(score1, score2);
 
 }
 
@@ -36,52 +37,58 @@ int compute_score(string word)
         int id = get_id(c);
         if (id == 0)
         {
+            // if c is a non alphabetic character score = score.
             score += 0;
         }
         else
         {
+            // else the letter's value is added to score.
             score += POINTS[id - 1];
         }
     }
     return score;
 }
 
+// Get the id of the character in alphabet.
+// Ex : get_id('a') --> 1
+//      get_id('c') --> 3
+
 int get_id(char c)
 {
     int id;
     if (isupper(c))
     {
+        // Uppercase alphabet start with 'A'(65).
         id = c - 64;
     }
     else if (islower(c) > 0)
     {
+        // Lowercase alphabet start with 'a'(95).
         id = c - 96;
     }
     else
     {
+        // For non alphabetic characters id = 0.
         id = 0;
     }
     return id;
 }
 
-int print_result(int score1, int score2)
+void print_result(int score1, int score2)
 {
     // Tie
     if (score1 == score2)
     {
         printf("Tie!\n");
-        return 0;
     }
     // Player 1 wins
     if (score1 > score2)
     {
         printf("Player 1 wins!\n");
-        return 0;
     }
     // Player 2 wins
     if (score1 < score2)
     {
         printf("Player 2 wins!\n");
-        return 0;
     }
 }
