64d20611b9b278dfa9d103a0bf5e114e6b79f36f
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-25 14:01:47 +0000

    Thu, May 25, 2023, 10:01 AM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,9 +1,28 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+
+int count_letters(string text);
 
 int main(void)
 {
     string text = get_string("Text : ");
-    printf("%s\n", text);
+    int count = count_letters(text);
+    printf("Number of letter: %i\n", count);
+}
+
+int count_letters(string text)
+{
+    int lenght = strlen(text);
+    int count = 0;
+    int i;
+    for (i = 0; i < lenght; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
 }
