409349ed3546ec927a8f72f362704001db07fc2a
Maxime Riera <125496580+MaxI6s@users.noreply.github.com>
2023-05-25 13:02:52 +0000

    Thu, May 25, 2023, 9:02 AM -04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -18,7 +18,23 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // Tie
+
+    if (score1 == score2)
+    {
+        printf("Tie!")
+        return 0
+    }
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!")
+        return 0
+    }
+    if (score1 < score2)
+    {
+        printf("Player 2 wins!")
+        return 0
+    }
 }
 
 int compute_score(string word)
